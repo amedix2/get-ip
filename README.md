c6379596f556ba6c872ff106c0511071fe9eb797
amedix-mac <amedix2@gmail.com>
2024-05-05 03:03:25 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # get-ip
+
+`pip3 install -r requirements.txt`
+
+`cp .env-example .env`  
+change the values to the appropriate ones in .`env`
---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import http.client
+import requests
+import time
+from datetime import datetime
+from dotenv import load_dotenv
+
+
+def send_message(token, chat_id, text):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    data = {"chat_id": chat_id, "text": text}
+    response = requests.post(url, data=data)
+    return response.json()
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+    TELEGRAM_CHAT_ID = int(os.getenv('TELEGRAM_CHAT_ID'))
+    DELAY = int(os.getenv('DELAY'))
+
+    conn = http.client.HTTPConnection("ifconfig.me")
+    while True:
+        conn.request("GET", "/ip")
+        r = conn.getresponse().read().decode()
+        msg = send_message(token=TELEGRAM_TOKEN, chat_id=TELEGRAM_CHAT_ID, text=f'{r}\n{datetime.now()}')
+        time.sleep(DELAY)
